4b2b200c0dad3b3dbf4a1c644a7d0eff6ab815e9
Joseph Kiragu <jkiragu@F2N021.42adel.org.au>
2022-10-22 15:59:14 +1030

    complete server file

--- server.c
+++ server.c
@@ -26,15 +26,17 @@ static void response(unsigned int *c_pid)
 	*c_pid = 0;
 }
 
-static void handler(int sig, siginfo_t *siginfo, void *optional)
+static void handler(int sig, siginfo_t *siginfo, void *context)
 {
+	// sig contains the system generated signal number
+	//*siginfo points to an object of type siginfo_t explaining the reason why the signal was generated
 	static unsigned char	c = 0;
 	static int 				i = 0;
 	static unsigned int		c_pid = 0;
 
-	(void)optional;
+	(void)context;
 	if (!c_pid)
-		c_pid = siginfo->si_pid;
+		c_pid = siginfo->si_pid; // si_pid is the sending process id
 	c |= (sig == SIGUSR1);
 	if (i++ == 8)
 	{
@@ -43,6 +45,8 @@ static void handler(int sig, siginfo_t *siginfo, void *optional)
 			return (response(&c_pid));
 		ft_putchar(c);
 		c = 0;
+		//kill() system call is used to send any signal to any process group or process
+		//SIGUSR2 is sent to the process defined by c_pid
 		kill(c_pid, SIGUSR2);
 	}
 	else
@@ -62,7 +66,10 @@ int main(void)
 	ft_putnbr(getpid());
 	ft_putstr("\n");
 
+	//pointer to a signal handling function
 	sa.sa_sigaction = handler;
+	// special flags to affect behavious of signal
+	// since SA_SIGINFO flag is set. sa_sigaction field specifies the signal catching function
 	sa.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa, 0) == -1)
 		return(EXIT_FAILURE);
