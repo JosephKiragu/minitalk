fb1dc9b6795a2381b0edfefe1d6eea9f69804247
Joseph Kiragu <jkiragu@F2N049.42adel.org.au>
2022-10-31 13:11:02 +1030

    code cleaning

--- client.c
+++ client.c
@@ -1,65 +1,10 @@
 #include "minitalk.h"
 
-// #include <signal.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-
-// void	ft_putstr(char *str)
-// {
-// 	while (*str)
-// 		write(1, str++, 1);
-// }
-
-// void	ft_putchar(char c)
-// {
-// 	write(1, &c, 1);
-// }
-
-// void	ft_putnbr(int n)
-// {
-// 	if (n > 9)
-// 		ft_putnbr(n / 10);
-// 	write(1, &"0123456789"[n % 10], 1);
-// }
-
-// int	ft_atoi(const char *str)
-// {
-// 	int			res;
-// 	int			sign;
-
-// 	sign = 1;
-// 	res = 0;
-// 	while (*str >= 7 && *str <= 14)
-// 		str++;
-// 	if (*str == '-')
-// 		sign = -1;
-// 	if (*str == '-' || *str == '+')
-// 		str++;
-// 	while (*str >= '0' && *str <= '9')
-// 	{
-// 		res = res * 10 + (*str - 48);
-// 		str++;
-// 	}
-// 	return (res * sign);
-// }
-
-// size_t	ft_strlen(const char *str)
-// {
-// 	int	i;
-
-// 	if (!str)
-// 		return (0);
-// 	i = 0;
-// 	while (str[i])
-// 		i++;
-// 	return (i);
-// }
-
-static void ft_client_handler(int signum, siginfo_t *siginfo, void *context)
+static void client_handler(int signum, siginfo_t *siginfo, void *context)
 {
 	(void)siginfo;
 	(void)context;
-	if (signum == SIGUSR2) // SIGUSR2 is the sending signal 
+	if (signum == SIGUSR2)
 	{
 		return ;
 	}
@@ -67,7 +12,7 @@ static void ft_client_handler(int signum, siginfo_t *siginfo, void *context)
 	exit(EXIT_FAILURE);	
 }
 
-static void ft_last_bit(int pid)
+static void last_bit(int pid)
 {
 	int i;
 
@@ -97,18 +42,18 @@ static void send_to_server(int pid, char *str)
 		c = *str++;
 		while (i--)
 		{	
-			if (c >> i & 1) // checks if bit is 1 else it's 0
+			if (c >> i & 1)
 			{
-				if(kill(pid, SIGUSR1) == -1) // kill function sends bit 1
+				if(kill(pid, SIGUSR1) == -1)
 					ft_error();
 			}
 			else
-				if (kill(pid, SIGUSR2) == -1) // kill function sends bit 0
+				if (kill(pid, SIGUSR2) == -1)
 					ft_error();
 			usleep(50);
 		}
 	}
-	ft_last_bit(pid);
+	last_bit(pid);
 }
 
 int main(int argc, char **argv)
@@ -120,7 +65,7 @@ int main(int argc, char **argv)
 		ft_putstr(" Enter arguments as: ./client [pid] [string]\n");
 		exit(0);
 	}
-	sa.sa_sigaction = ft_client_handler;
+	sa.sa_sigaction = client_handler;
 	sa.sa_flags =SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa, 0) == -1)
 		exit(EXIT_FAILURE);
--- server.c
+++ server.c
@@ -1,45 +1,21 @@
 #include "minitalk.h"
 
-// #include <signal.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-
-// void	ft_putchar(char c)
-// {
-// 	write(1, &c, 1);
-// }
-
-// void	ft_putstr(char *str)
-// {
-// 	while (*str)
-// 		write(1, str++, 1);
-// }
-
-// void	ft_putnbr(int n)
-// {
-// 	if (n > 9)
-// 		ft_putnbr(n / 10);
-// 	write(1, &"0123456789"[n % 10], 1);
-// }
-
 static void response(unsigned int *c_pid)
 {
 	kill(*c_pid, SIGUSR1);
 	*c_pid = 0;
 }
 
-static void handler(int sig, siginfo_t *siginfo, void *context)
+static void server_handler(int signum, siginfo_t *siginfo, void *context)
 {
-	// sig contains the system generated signal number
-	//*siginfo points to an object of type siginfo_t explaining the reason why the signal was generated
 	static unsigned char	c = 0;
 	static int 				i = 0;
 	static unsigned int		c_pid = 0;
 
 	(void)context;
 	if (!c_pid)
-		c_pid = siginfo->si_pid; // si_pid is the sending process id
-	c |= (sig == SIGUSR1); //BITWISE OR OPERATION WITH 0
+		c_pid = siginfo->si_pid;
+	c |= (signum == SIGUSR1);
 	if (++i == 8)
 	{
 		i = 0;
@@ -47,15 +23,11 @@ static void handler(int sig, siginfo_t *siginfo, void *context)
 			return (response(&c_pid));
 		ft_putchar(c);
 		c = 0;
-		//kill() system call is used to send any signal to any process group or process
-		//SIGUSR2 is sent to the process defined by c_pid
-		kill(c_pid, SIGUSR2); //SIGUSR2 is the signal being sent
+		kill(c_pid, SIGUSR2);
 	}
 	else
 		c = c << 1;
 }
-
-
 
 int main(void)
 {
@@ -65,11 +37,7 @@ int main(void)
 	ft_putstr("PID : \n");
 	ft_putnbr(getpid());
 	ft_putstr("\n");
-
-	//pointer to a signal handling function
-	sa.sa_sigaction = handler;
-	// special flags to affect behavious of signal
-	// since SA_SIGINFO flag is set. sa_sigaction field specifies the signal catching function
+	sa.sa_sigaction = server_handler;
 	sa.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa, 0) == -1)
 		return(EXIT_FAILURE);
