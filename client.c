143d11578bbd83f1786ce85bc652b488c028fdc3
Joseph Kiragu <jkiragu@F2N021.42adel.org.au>
2022-11-03 15:14:53 +1030

    adding header

--- client.c
+++ client.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkiragu <jkiragu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 15:13:14 by jkiragu           #+#    #+#             */
+/*   Updated: 2022/11/03 15:13:46 by jkiragu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-static void client_handler(int signum, siginfo_t *siginfo, void *context)
+static void	client_handler(int signum, siginfo_t *siginfo, void *context)
 {
 	(void)siginfo;
 	(void)context;
@@ -9,12 +21,12 @@ static void client_handler(int signum, siginfo_t *siginfo, void *context)
 		return ;
 	}
 	ft_putstr("sending successful");
-	exit(EXIT_FAILURE);	
+	exit(EXIT_FAILURE);
 }
 
-static void last_bit(int pid)
+static	void	last_bit(int pid)
 {
-	int i;
+	int	i;
 
 	i = 8;
 	while (i--)
@@ -25,18 +37,18 @@ static void last_bit(int pid)
 	}
 }
 
-static void ft_error(void)
+static	void	ft_error(void)
 {
 	ft_putstr("\nError :Invalid PID\n");
 	exit(EXIT_FAILURE);
 }
 
-static void send_to_server(int pid, char *str)
+static	void	send_to_server(int pid, char *str)
 {
 	int		i;
 	char	c;
 
-	while(*str)
+	while (*str)
 	{
 		i = 8;
 		c = *str++;
@@ -44,7 +56,7 @@ static void send_to_server(int pid, char *str)
 		{	
 			if (c >> i & 1)
 			{
-				if(kill(pid, SIGUSR1) == -1)
+				if (kill(pid, SIGUSR1) == -1)
 					ft_error();
 			}
 			else
@@ -56,17 +68,17 @@ static void send_to_server(int pid, char *str)
 	last_bit(pid);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
-	if(argc !=3 || !ft_strlen(argv[2]))
+	if (argc != 3 || !ft_strlen(argv[2]))
 	{
 		ft_putstr(" Enter arguments as: ./client [pid] [string]\n");
 		exit(0);
 	}
 	sa.sa_sigaction = client_handler;
-	sa.sa_flags =SA_SIGINFO;
+	sa.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa, 0) == -1)
 		exit(EXIT_FAILURE);
 	if (sigaction(SIGUSR2, &sa, 0) == -1)
